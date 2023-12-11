b2b0980ef5022cb3af1a502d72a201283a3371d3
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-12-11 10:58:20 +0630

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import Sidebar from "./shared/SideBar.jsx";
 import TableComponent from "./shared/Table.jsx";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./AppRouter.jsx";
+import { Routes, Route } from "react-router-dom";
+import Customer from "./pages/customer";
+import Factory from "./pages/factory";
+import Sales from "./pages/sales";
+import Logstic from "./pages/logstic";
+import Warehouse from "./pages/warehouse";
+import Admin from "./pages/admin";
 
 function App() {
   return (
     <div>
-      <RouterProvider router={router} />
-
       <Row>
         <Col xs={3}>
           <Sidebar />
         </Col>
         <Col xs={9}>
           <TableComponent />
-          <h1>Hello</h1>
+          <Routes>
+            <Route path="/" element={<Sidebar />} />
+            <Route path="/factory" element={<Factory />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/logstic" element={<Logstic />} />
+            <Route path="/customer" element={<Customer />} />
+            <Route path="/warehouse" element={<Warehouse />} />
+            <Route path="/sales" element={<Sales />} />
+          </Routes>
         </Col>
       </Row>
     </div>

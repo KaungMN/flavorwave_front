11b0e50f351996ef94eb6be47fa9a858d801ce12
KaungMN <kaungmyatnoe2016@gmail.com>
2023-12-11 16:22:23 +0630

    Customer Login, Signup & Product

--- src/components/customers/CustomerProductPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CustomerProductPage() {
-  return <div>CustomerProductPage</div>;
-}
-
-export default CustomerProductPage;
---
+++ src/components/customers/Products.jsx
@@ -0,0 +1,116 @@
+export const products = [
+  {
+    name: "Burmese Bliss",
+    description:
+      "Experience the blissful taste of Burma with this exquisite tea.",
+    price: 14.99,
+  },
+  {
+    name: "Golden Sunshine Tea",
+    description:
+      "Brighten your day with the warmth of golden sunshine in every sip.",
+    price: 12.99,
+  },
+  {
+    name: "Mango Tango Delight",
+    description: "Dance into delight with the tropical flavors of mango tango.",
+    price: 16.99,
+  },
+  {
+    name: "Rangoon Rosewater Elixir",
+    description:
+      "Indulge in the elegance of Rangoon with this rosewater elixir.",
+    price: 18.99,
+  },
+  {
+    name: "Emerald Green Chai",
+    description: "Savor the rich and aromatic blend of emerald green chai.",
+    price: 15.99,
+  },
+  {
+    name: "Citrus Fusion Fizz",
+    description:
+      "Experience a burst of citrus flavors with this refreshing fizz.",
+    price: 13.99,
+  },
+  {
+    name: "Coconut Cream Dream",
+    description:
+      "Take a sip and let the coconut cream transport you to a dreamy paradise.",
+    price: 17.99,
+  },
+  {
+    name: "Jasmine Serenade Soda",
+    description:
+      "Let the enchanting aroma of jasmine serenade your senses in every sip.",
+    price: 14.99,
+  },
+  {
+    name: "Papaya Paradise Punch",
+    description:
+      "Immerse yourself in the tropical paradise with this papaya punch.",
+    price: 16.99,
+  },
+  {
+    name: "Lychee Lullaby",
+    description: "Fall into a sweet lullaby with the delicate taste of lychee.",
+    price: 15.99,
+  },
+  {
+    name: "Tropical Twilight Nectar",
+    description:
+      "Experience the magic of tropical twilight with this delightful nectar.",
+    price: 19.99,
+  },
+  {
+    name: "Orchid Oasis Euphoria",
+    description: "Find euphoria in the orchid oasis with this exquisite drink.",
+    price: 18.99,
+  },
+  {
+    name: "Starfruit Sparkling Sorbet",
+    description:
+      "Sparkle with delight as you enjoy the starfruit-infused sparkling sorbet.",
+    price: 14.99,
+  },
+  {
+    name: "Ginger Zing Zest",
+    description:
+      "Awaken your taste buds with the invigorating ginger zing zest.",
+    price: 12.99,
+  },
+  {
+    name: "Lush Lemongrass Infusion",
+    description:
+      "Indulge in the lushness of lemongrass in this refreshing infusion.",
+    price: 16.99,
+  },
+  {
+    name: "Ruby Red Guava Gusto",
+    description: "Satisfy your gusto with the vibrant taste of ruby red guava.",
+    price: 17.99,
+  },
+  {
+    name: "Blueberry Burst Breeze",
+    description: "Feel the breezy burst of blueberry freshness in every sip.",
+    price: 13.99,
+  },
+  {
+    name: "Pineapple Pizzazz Quencher",
+    description:
+      "Quench your thirst with the zesty pineapple pizzazz quencher.",
+    price: 15.99,
+  },
+  {
+    name: "Passionfruit Pomegranate Bliss",
+    description:
+      "Experience bliss with the passionate fusion of passionfruit and pomegranate.",
+    price: 18.99,
+  },
+  {
+    name: "Exotic Cucumber Limeade",
+    description:
+      "Sip on the exotic blend of cucumber and limeade for a refreshing experience.",
+    price: 16.99,
+  },
+];

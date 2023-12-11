2962c4b1c0913f508040695245591b0cabcd45ba
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-12-11 13:34:38 +0630

    added pagination logic and truck modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Sidebar from "./shared/SideBar.jsx";
-import TableComponent from "./shared/Table.jsx";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Routes, Route } from "react-router-dom";
@@ -10,7 +9,7 @@ import Factory from "./pages/factory";
 import Sales from "./pages/sales";
 import Logstic from "./pages/logstic";
 import Warehouse from "./pages/warehouse";
-import Admin from "./pages/Admin.jsx";
+import Admin from "./pages/admin";
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Sidebar />
         </Col>
         <Col xs={9}>
-          <TableComponent />
           <Routes>
             <Route path="/" element={<Sidebar />} />
             <Route path="/factory" element={<Factory />} />

0245eab2424a01236546c7e79d94707992fa18cd
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-12-12 14:09:44 +0630

    created cards to buy raw material

--- src/pages/Factory.jsx
+++ src/pages/Factory.jsx
@@ -1,11 +1,13 @@
-import SelectRaw from "../components/factory/SelectRaw";
+import RawCard from "../components/factory/RawCard";
 import { Container } from "react-bootstrap";
 
 function factory() {
-  return <Container>
-    <h2>Factory</h2>
-    <SelectRaw/>
-  </Container>;
+  return (
+    <Container>
+      <h2>Factory</h2>
+      <RawCard />
+    </Container>
+  );
 }
 
 export default factory;

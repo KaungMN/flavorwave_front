cf8be426fffd9b4c941f584adbd35889e6e380ec
Htet Wonna Oo <nitoe1999@gmail.com>
2023-12-13 11:47:53 +0630

    multiselect table

--- src/pages/Factory/Factory.jsx
+++ src/pages/Factory/Factory.jsx
@@ -1,9 +1,11 @@
 import TableComponent from "../../components/factory/product/table/Table";
 import { Container } from "react-bootstrap";
+import MultiSelectTable from "./SelectTable";
 
 function factory() {
   return (
     <Container>
+      <MultiSelectTable />
       <h2>Factory</h2>
       <TableComponent />
     </Container>

b82e00de74019ab2974855aa9acb0a5c26c67eab
Htet Wonna Oo <nitoe1999@gmail.com>
2023-12-14 11:08:45 +0630

    turck table

--- src/components/logistic/truck-table/SingleRow.jsx
+++ src/components/logistic/truck-table/SingleRow.jsx
@@ -1,6 +1,8 @@
+import { Button } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
-
-const heading = ['Product Name', 'Box', 'Pcs', 'Price', 'expire-date'];
+import Edit from './EditProduct';
+import Delete from './Delete';
+const heading = ['Truck Name', 'Truck Number', 'Capacity', 'Staff Id'];
 
 export default function TableSingleRow({ order, handleDelete }) {
     let staffId = JSON.parse(sessionStorage.getItem('staffId'));
@@ -19,14 +21,18 @@ export default function TableSingleRow({ order, handleDelete }) {
                 {order.map((item, id) => (
                     <tr key={id}>
                         <td>{id + 1}</td>
-                        <td key={item.orderItem}>{item.orderItem}</td>
-                        <td key={item.orderQuantity}>{item.orderQuantity}</td>
-                        <td key={item.salesPermit}>{item.salesPermit}</td>
-                        <td key={item.sales}>{item.sales}</td>
-                        <td key={item.Permit}>{item.Permit}</td>
-                        {staffId >= 2 ? <td></td> : null}
+                        <td >{item.truck_name}</td>
+                        <td >{item.truck_number}</td>
+                        <td >{item.capacity}</td>
+                        <td >{item.staff_id}</td>
+                        {staffId >= 2 ? (
+                            <td>
+                                <Edit initialData={item} />
+                                <Delete handleDelete={handleDelete} id={item.orderItem} />
+                            </td>
+                        ) : null}
                     </tr>
-                ))}
+                ))} 
             </tbody>
         </Table>
     );
